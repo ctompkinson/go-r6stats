4186711e047732362ca2351b69024f53d3b8bb64
Chris Tompkinson <ctompkinson@gmail.com>
2018-03-04 23:00:15 +0000

    add operator get information

--- client.go
+++ client.go
@@ -12,7 +12,7 @@ func NewClient(httpClient http.Client) (client) {
 	return client{httpClient}
 }
 
-func (c *client) GetPlayer (playerName string, platform string) (Player, error){
+func (c *client) GetPlayer (playerName string, platform string, operators bool) (Player, error){
 	playerUrl := c.GetPlayerUrl(playerName, platform)
 	res, err := c.httpClient.Get(playerUrl)
 
@@ -36,9 +36,60 @@ func (c *client) GetPlayer (playerName string, platform string) (Player, error){
 	}
 
 	res.Body.Close()
+
+
+	// If we're getting the operator information to an additional get for the operator information
+	if operators {
+		c.getOperators(playerName, platform, &playerRes.Player)
+	}
+
 	return playerRes.Player, nil
 }
 
+// Takes an existing player and populates it with operator information
+func (c *client) getOperators(playerName string, platform string, player *Player) error {
+	operatorUrl := c.GetPlayerOperatorsUrl(playerName, platform)
+	res, err := c.httpClient.Get(operatorUrl)
+
+	if err != nil {
+		return errors.Wrap(err, "unable to retrieve player")
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New(fmt.Sprintf("got status %s when retrieving operators", strconv.Itoa(res.StatusCode)))
+	}
+
+	var operatorResponse OperatorResponse
+	decoder := json.NewDecoder(res.Body)
+	resErr := decoder.Decode(operatorResponse)
+	if resErr != nil {
+		fmt.Println(resErr)
+		return errors.Wrap(err, "Unable to retrieve operators")
+	}
+	res.Body.Close()
+
+	for _, op := range operatorResponse.OperatorRecords {
+		player.Operators[op.Operator.Name] = Operator{
+			Name:     op.Operator.Name,
+			Role:     op.Operator.Role,
+			Wins:     op.Stats.Wins,
+			Losses:   op.Stats.Losses,
+			Kills:    op.Stats.Kills,
+			Deaths:   op.Stats.Deaths,
+			Playtime: op.Stats.Playtime,
+			Specials: op.Stats.Specials,
+			Images:   op.Operator.Images,
+		}
+	}
+
+	return nil
+}
+
+
 func (c *client) GetPlayerUrl (player string, platform string) string {
 	return fmt.Sprintf(Endpoint + "/players/%s?platform=%s", player, platform)
+}
+
+func (c *client) GetPlayerOperatorsUrl (player string, platform string) string {
+	return fmt.Sprintf(Endpoint + "/players/%s/operators?platform=%s", player, platform)
 }
--- model.go
+++ model.go
@@ -13,46 +13,91 @@ type PlayerResponse struct {
 }
 
 type Player struct {
-	Username string `json:"username"`
-	Platform string `json:"platform"`
+	Username  string `json:"username"`
+	Platform  string `json:"platform"`
 	UbisoftId string `json:"ubisoft_id"`
 	IndexedAt string `json:"indexed_at"`
 	UpdatedAt string `json:"updated_at"`
-	Stats Stats `json:"stats"`
+	Stats     Stats  `json:"stats"`
+	Operators map[string]Operator
 }
 
 type Stats struct {
-	Ranked GameTypeStats `json:"ranked"`
-	Casual GameTypeStats `json:"casual"`
-	Overall OverallStats `json:"overall"`
+	Ranked      GameTypeStats    `json:"ranked"`
+	Casual      GameTypeStats    `json:"casual"`
+	Overall     OverallStats     `json:"overall"`
 	Progression ProgressionStats `json:"progression"`
 }
 
 type GameTypeStats struct {
-	HasPlayed bool `json:"has_played"`
-	Wins int `json:"wins"`
-	Losses int `json:"losses"`
-	WinLossRatio float64 `json:"wlr"`
-	Kills int `json:"kills"`
-	Deaths int `json:"deaths"`
+	HasPlayed      bool    `json:"has_played"`
+	Wins           int     `json:"wins"`
+	Losses         int     `json:"losses"`
+	WinLossRatio   float64 `json:"wlr"`
+	Kills          int     `json:"kills"`
+	Deaths         int     `json:"deaths"`
 	KillDeathRatio float64 `json:"kd"`
-	Playtime float64 `json:"playtime"`
+	Playtime       float64 `json:"playtime"`
 }
 
 type OverallStats struct {
-	Revives int `json:"revives"`
-	Suicides int `json:"suicides"`
+	Revives                int `json:"revives"`
+	Suicides               int `json:"suicides"`
 	ReinforcementsDeployed int `json:"reinforcements_deployed"`
-	BarricadesBuilt int `json:"barricades_built"`
-	StepsMoved int `json:"steps_moved"`
-	BulletsFired int `json:"bullets_fired"`
-	Headshots int `json:"headshots"`
-	MeleeKills int `json:"melee_kills"`
-	PenetrationKills int `json:"penetration_kills"`
-	Assists int `json:"assists"`
+	BarricadesBuilt        int `json:"barricades_built"`
+	StepsMoved             int `json:"steps_moved"`
+	BulletsFired           int `json:"bullets_fired"`
+	Headshots              int `json:"headshots"`
+	MeleeKills             int `json:"melee_kills"`
+	PenetrationKills       int `json:"penetration_kills"`
+	Assists                int `json:"assists"`
 }
 
 type ProgressionStats struct {
 	Level int `json:"level"`
-	Xp int `json:"xp"`
+	Xp    int `json:"xp"`
+}
+
+type OperatorResponse struct {
+	OperatorRecords []OperatorRecord `json:"operator_records"`
+}
+
+type OperatorRecord struct {
+	Stats    OperatorRecordStats `json:"stats"`
+	Operator OperatorRecordInfo  `json:"operator"`
+}
+
+type OperatorRecordStats struct {
+	Played   int               `json:"played"`
+	Wins     int               `json:"wins"`
+	Losses   int               `json:"losses"`
+	Kills    int               `json:"kills"`
+	Deaths   int               `json:"deaths"`
+	Playtime int               `json:"playtime"`
+	Specials map[string]string `json"specials"`
+}
+
+type OperatorRecordInfo struct {
+	Name   string `json"name"`
+	Role   string `json:"role"`
+	Ctu    string `json:"ctu"`
+	Images Images `json:"images"`
+}
+
+type Operator struct {
+	Name     string            `json:"name"`
+	Role     string            `json:"role"`
+	Wins     int               `json:"wins"`
+	Losses   int               `json:"losses"`
+	Kills    int               `json:"kills"`
+	Deaths   int               `json:"deaths"`
+	Playtime int               `json:"playtime"`
+	Specials map[string]string `json:"specials"`
+	Images   Images            `json:"images"`
+}
+
+type Images struct {
+	Figure string `json:"figure"`
+	Badge  string `json:"badge"`
+	Bust   string `json:"bust"`
 }
